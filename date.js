3d761b05f7c7812374c1174d66c4717a14f53d26
Mohamed Magde Mohamed <mohamedmagdemohamed@MacBook-Pro.local>
2020-06-26 16:08:34 +0300

    first committ

---
+++ date.js
@@ -0,0 +1,20 @@
+/*
+Write a JavaScript program to get the current date.
+Expected Output :
+mm-dd-yyyy, mm/dd/yyyy or dd-mm-yyyy, dd/mm/yyyy
+ */
+var today = new Date();
+var dd = today.getDay();
+var mm = today.getMonth();
+var yyyy = today.getFullYear();
+
+if (dd < 10) {
+  dd = "0" + dd;
+}
+
+if (mm < 10) {
+  mm = "0" + mm;
+}
+
+today = mm + "/" + dd + "/" + yyyy;
+console.log(today);
